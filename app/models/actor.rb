46981bb1c8130c0981e4d61c3444c6352b539838
Jeanette Rosario <jeanetterosario88@gmail.com>
2020-06-14 22:18:50 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -9,7 +9,7 @@ class Actor < ActiveRecord::Base
 
   def list_roles
     self.characters.collect do |eachcharacter|
-      "'#{eachcharacter} - #{eachcharacter.show.name}'"
+      "'#{eachcharacter.name} - #{eachcharacter.show.name}'"
     end
   end
 
