6b3cf8b4cd30716fd344efe6671e142f54fbc2ab
Jeanette Rosario <jeanetterosario88@gmail.com>
2020-06-14 22:33:22 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,4 +1,11 @@
 class Show < ActiveRecord::Base
   belongs_to :network
   has_many :characters
+
+def actors_list
+  show.characters.map do |eachcharacter|
+    eachcharacter.actor.full_name
+  end
+end
+  
 end
